52a46bb531cc81c38d6ea6bf96384566e9357bea
peke-pon <toyo718@gmail.com>
2020-02-19 22:41:38 +0900

    eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { join } from 'lodash-es';
 
-import "./scss/style.scss";
+import './scss/style.scss';
 
-import img from "./images/webpack.svg";
-import img2 from "./images/bg.svg";
+import img from './images/webpack.svg';
+import img2 from './images/bg.svg';
 
 const image = new Image();
 image.src = img;
@@ -13,7 +13,7 @@ const image2 = new Image();
 image2.src = img2;
 document.body.appendChild(image2);
 
-const array = ["web", "pack",];
+const array = ['web', 'pack'];
 const elm = document.createElement('p');
 elm.innerHTML = join(array, '');
 document.body.appendChild(elm);
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,91 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = (env, argv) => {
   const MODE = argv.mode;
-  const development = MODE === "development";
-  const outputPath = path.resolve(__dirname, "dist");
+  const development = MODE === 'development';
+  const outputPath = path.resolve(__dirname, 'dist');
 
   return {
     mode: MODE,
-    entry: "./src/index.js",
+    entry: './src/index.js',
     output: {
       path: outputPath,
-      filename: development ? "[name].[hash].js" : "main.min.js"
+      filename: development ? '[name].[hash].js' : 'main.min.js',
     },
     module: {
       rules: [
         {
           test: /\.(jpe?g|png|gif|svg|ico)$/i,
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 51200,
-            name: "./images/[name].[ext]"
-          }
+            name: './images/[name].[ext]',
+          },
         },
         {
           test: /\.(sc|c|sa)ss$/,
           use: [
-            development ? MiniCssExtractPlugin.loader : "style-loader",
+            development ? MiniCssExtractPlugin.loader : 'style-loader',
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
                 url: false,
-                sourceMap: development
-              }
+                sourceMap: development,
+              },
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
-                sourceMap: development
-              }
-            }
-          ]
+                sourceMap: development,
+              },
+            },
+          ],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      ]
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [['@babel/preset-env', { modules: false }]],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          loader: 'eslint-loader',
+          enforce: 'pre',
+          exclude: /node_modules/,
+        },
+      ],
     },
     plugins: [
       new HtmlWebpackPlugin(),
       new MiniCssExtractPlugin({
-        filename: development ? "[name].[hash].css" : "style.css"
-      })
+        filename: development ? '[name].[hash].css' : 'style.css',
+      }),
     ],
     optimization: {
       minimizer: development
         ? []
         : [
-            new TerserPlugin({
-              terserOptions: {
-                extractComments: "all",
-                compress: { drop_console: true }
-              }
-            })
-          ]
+          new TerserPlugin({
+            terserOptions: {
+              compress: { drop_console: true },
+            },
+          }),
+        ],
     },
     devServer: {
       contentBase: outputPath,
       port: 3000,
-      open: true
+      open: true,
     },
-    devtool: "eval-source-map"
+    devtool: 'eval-source-map',
   };
 };
